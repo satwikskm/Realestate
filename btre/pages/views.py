047477e8c350e5b2fd8697de995e983322fa98df
S@twit <satwikmohantyskm@gmail.com>
2021-05-09 00:47:35 +0530

    Sixth Commit

--- btre/pages/views.py
+++ btre/pages/views.py
@@ -1,10 +1,25 @@
+from listings.views import listing
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
+from realtors.models import Realtor
 # Create your views here.
 
 def index(request):
-    return render(request,'pages/index.html')
+    listings =Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context={
+        'listings':listings
+    }
+    return render(request,'pages/index.html',context)
 
 def about(request):
+    #Get all realtors
+    realtors=Realtor.objects.order_by("-hire_date")
+    #get mvp
+    mvp_realtors=Realtor.objects.all().filter(is_mvp=True)
     
-    return render(request,'pages/about.html')
+    context={
+        'realtors':realtors,
+        'mvp_realtors':mvp_realtors
+    }
+    return render(request,'pages/about.html',context)
